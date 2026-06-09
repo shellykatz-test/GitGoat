928148d9d28b3f5d24e03fcb99aa3cb40c5786a0
Michael Sparace <engineering@arnica.io>
2026-06-09 17:49:11 -0400

    Add file download endpoint

---
+++ src/api/vuln_download4.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+app.get("/download", (req, res) => {
+  const filePath = path.join("./public", req.query.file);
+  const data = fs.readFileSync(filePath);
+  res.send(data);
+});
+module.exports = app; 
